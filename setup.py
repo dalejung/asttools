476b164d1c52ca452a9224f67002c5e7faa0be7e
Dale Jung <dale@dalejung.com>
2013-06-25 10:39:58 -0400

    inital setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup                                                      
+                                                                                      
+DISTNAME='asttools'                                                                 
+FULLVERSION='0.1'                                                                     
+                                                                                      
+setup(name=DISTNAME,                                                                  
+      version=FULLVERSION,                                                            
+      packages=['asttools',
+               ]                                                         
+      )   
