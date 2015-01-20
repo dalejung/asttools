e7571fa1286747221e89ce551c00efbf358aae9c
Dale Jung <dale@dalejung.com>
2015-01-19 21:03:06 -0500

    split up eval into compile as well

--- asttools/eval.py
+++ asttools/eval.py
@@ -1,4 +1,5 @@
 import ast
+import logging
 
 from .common import _convert_to_expression
 from .repr import ast_repr
@@ -9,6 +10,20 @@ def _exec(node, ns):
 
     Fall back to exec
     """
+    code = _compile(node)
+    res = eval(code, ns)
+    return res
+
+def _eval(node, ns):
+    """
+    Will eval an ast Node within a namespace.
+    """
+    expr = _convert_to_expression(node)
+    if expr is None:
+        raise Exception("{0} cannot be evaled".format(repr(node)))
+    return _exec(node, ns)
+
+def _compile(node, force_eval=False):
     node = ast.fix_missing_locations(node)
 
     mode = 'exec'
@@ -22,16 +37,5 @@ def _exec(node, ns):
         module = expr
         mode = 'eval'
 
-    print('eval', ast_repr(node), mode)
     code = compile(module, '<dale>', mode)
-    res = eval(code, ns)
-    return res
-
-def _eval(node, ns):
-    """
-    Will eval an ast Node within a namespace.
-    """
-    expr = _convert_to_expression(node)
-    if expr is None:
-        raise Exception("{0} cannot be evaled".format(repr(node)))
-    return _exec(node, ns)
+    return code
