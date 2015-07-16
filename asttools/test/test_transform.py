0dda21f39a30a3f3fbfa4bfe09151d9e0883f613
Dale Jung <dale@dalejung.com>
2015-07-15 20:31:02 -0400

    Added node transformer

---
+++ asttools/test/test_transform.py
@@ -0,0 +1,47 @@
+import ast
+import astor
+from unittest import TestCase
+from textwrap import dedent
+
+import nose.tools as nt
+
+from ..transform import (
+    transform,
+    NodeTransformer
+)
+
+def test_name_rename():
+    """
+    Test a simple transformer to rename
+    """
+    class Renamer(NodeTransformer):
+        def visit_Name(self, node, meta):
+            node.id = node.id + '_visited'
+            return node
+
+    renamer = Renamer()
+    mod = ast.parse("bob = frank")
+    transform(mod, renamer)
+    bob_node = mod.body[0].targets[0]
+    frank_node = mod.body[0].value
+
+    nt.assert_equals(bob_node.id, "bob_visited")
+    nt.assert_equals(frank_node.id, "frank_visited")
+
+def test_data_renamer():
+    class DataRenamer(NodeTransformer):
+        """
+        Rewrite `bob = frank` to `data['bob'] = data['frank']`
+        """
+        def visit_Name(self, node, meta):
+            return ast.copy_location(ast.Subscript(
+                        value=ast.Name(id='data', ctx=ast.Load()),
+                        slice=ast.Index(value=ast.Str(s=node.id)),
+                        ctx=node.ctx
+                    ), node)
+
+    renamer = DataRenamer()
+    mod = ast.parse("bob = frank")
+    transform(mod, renamer)
+    new_source = astor.to_source(mod)
+    nt.assert_equals(new_source, "data['bob'] = data['frank']")
---
+++ asttools/transform.py
@@ -0,0 +1,51 @@
+import ast
+import astor
+from ast import AST
+from textwrap import dedent
+from .graph import graph_walk, NodeLocation
+
+
+class NodeTransformer:
+    def visit(self, node, meta):
+        method = 'visit_' + node.__class__.__name__
+        visitor = getattr(self, method, self.generic_visit)
+        node_item = visitor(node, meta)
+        return node_item
+
+    def generic_visit(self, node, meta):
+        return node
+
+
+def transform(root, nt):
+    """
+    Largely taken from the ast source. Works a bit differently because
+    it depends on the graph_walk which returns items leaf first and then to
+    root.
+    """
+    gen = graph_walk(root)
+    done = {}
+
+    for item in gen:
+        node = item['node']
+        new_node = nt.visit(node, item)
+
+        for field_name, old_value in ast.iter_fields(node):
+            old_value = getattr(node, field_name, None)
+
+            if isinstance(old_value, list):
+                new_values = []
+                for field_index, value in enumerate(old_value):
+                    done_node = done.get(value)
+                    if done_node:
+                        new_values.append(done_node)
+                old_value[:] = new_values
+
+            elif isinstance(old_value, ast.AST):
+                done_node = done.get(old_value)
+                if done_node is None:
+                    delattr(node, field_name)
+                else:
+                    setattr(node, field_name, done_node)
+
+        done[node] = new_node
+    return root
