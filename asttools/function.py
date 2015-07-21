78472cf60c3d182ba56cb5aa567d8f65640b8675
Dale Jung <dale@dalejung.com>
2015-07-21 16:16:29 -0400

    function building tools

---
+++ asttools/function.py
@@ -0,0 +1,52 @@
+import ast
+import inspect
+import nose.tools as nt
+
+from . import get_source, quick_parse
+
+def create_function(code, func=None,
+                    globals=None,
+                    filename=None):
+    """
+    Creates a function object without touching any existing namespaces. Using
+    something like types.FunctionType doesn't work because you cannot compile
+    a code obj with a return statement.
+
+    This function is meant for creating functions to replace existing functions
+    via decorators.
+
+    code : ast.AST, str
+        code or source string that includes only a function definition
+    func : Function
+        will be used as a template for filename and globals if they are not
+        passed in
+    globals : dict
+        env function will be executed in
+    filename : str
+        filename attached to code object and used for debug statements
+    """
+    if func:
+        filename = inspect.getfile(func)
+        globals = globals or func.__globals__
+
+    if filename is None:
+        filename = '<asttools.function.create_function>'
+
+    if isinstance(code, str):
+        code = ast.parse(code)
+
+    func_def = code.body[0]
+    func_name = func_def.name
+    code_obj = compile(code, filename, 'exec')
+
+    ns = {}
+    exec(code_obj, globals, ns)
+    new_func = ns[func_name]
+    return new_func
+
+def wrap(transform):
+    def _wrapper(func):
+        code = ast.parse(get_source(func))
+        transform(code)
+        return create_function(code, func=func)
+    return _wrapper
---
+++ asttools/test/test_function.py
@@ -0,0 +1,56 @@
+import ast
+
+import nose.tools as nt
+
+from .. import get_source, quick_parse, Matcher
+from ..function import create_function, wrap
+from ..transform import coroutine, transform
+from ..graph import iter_fields
+
+def test_create_function():
+    # grab ast.walk
+    new_func = create_function("def walk(): return 10", ast.walk)
+    nt.assert_equals(new_func.__module__, 'ast') # module is retained
+    nt.assert_equals(new_func(), 10)
+
+
+    def some_func(bob):
+        return bob
+    # modify function to add 5
+    code = ast.parse(get_source(some_func))
+    code.body[0].body.insert(0, quick_parse("bob += 5"))
+    add_5 = create_function(code, func=some_func)
+
+    nt.assert_equals(some_func(10), 10)
+    nt.assert_equals(add_5(10), 15)
+
+def test_create_function_globals():
+    new_func = create_function("def walk(): return AST", ast.walk)
+    # should grab AST form the ast.walk global namespace
+    nt.assert_is(new_func(), ast.AST)
+
+    new_func2 = create_function("def walk(): return AST", ast.walk,
+                                globals={'AST':1})
+    nt.assert_equal(new_func2(), 1)
+
+def test_wrap():
+    def capture_transform(code):
+        @coroutine.wrap
+        def _transform():
+            node, meta = yield
+            matcher = Matcher("'<any>'.capture()")
+            while True:
+                if matcher.match(node):
+                    node.kwargs = quick_parse("locals()").value
+                    node.func.attr = 'format'
+
+                node, meta = yield node
+            return node
+        return transform(code, _transform())
+
+    @wrap(capture_transform)
+    def hello(obj):
+        end = 'goodbye'
+        return "hello {obj}... {end}".capture()
+
+    nt.assert_equal(hello('bob'), 'hello bob... goodbye')
