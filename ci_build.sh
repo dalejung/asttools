5ca39b3c00d984d9f81cea1367fd5bd784bb3fb2
Dale Jung <dale@dalejung.com>
2018-08-09 15:12:40 -0400

    ci

--- ci_build.sh
+++ ci_build.sh
@@ -2,6 +2,7 @@
 
 pip install pandas
 pip install numpy
-pip install git+https://github.com/berkerpeksag/astor.git
 pip install typeguard
+pip install git+https://github.com/berkerpeksag/astor.git
+pip install git+https://github.com/dalejung/earthdragon.git
 pip install .
